a70f14f08b3052baaec171e89fa99d1d1228292c
ben-denham <bend@catalyst.net.nz>
2014-01-17 16:46:57 +1300

    Fixed postgresql dev package name.

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -11,7 +11,7 @@ sudo apt-get install -yq python2.7 > /dev/null
 sudo apt-get install -yq python-pip > /dev/null
 sudo apt-get install -yq git > /dev/null
 sudo apt-get install -yq pep8 > /dev/null
-sudo apt-get install -yq postgresql-devel
+sudo apt-get install -yq libpq-dev
 
 echo '-- Installing pre-requisite pip packages'
 sudo pip install -q virtualenvwrapper
